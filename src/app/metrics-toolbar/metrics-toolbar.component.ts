004caaceed104c6be9b40b3363aac921ded179ce
Rob <null>
2016-07-06 20:19:58 +0000

    Initial commit

---
+++ src/app/metrics-toolbar/metrics-toolbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-metrics-toolbar',
+  templateUrl: 'metrics-toolbar.component.html',
+  styleUrls: ['metrics-toolbar.component.css']
+})
+export class MetricsToolbarComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/tab-bar/tab-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-tab-bar',
+  templateUrl: 'tab-bar.component.html',
+  styleUrls: ['tab-bar.component.css']
+})
+export class TabBarComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
